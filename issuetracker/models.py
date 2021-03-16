aebc8af9168eb9ea1ac823a14f440ee2cf05710a
Marcin <persky03@gmail.com>
2021-03-16 15:54:00 +0100

    Issue model

--- issuetracker/models.py
+++ issuetracker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+class Issue(models.Model):
+    issue_title = models.CharField(max_length=200)
+    issue_body = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
