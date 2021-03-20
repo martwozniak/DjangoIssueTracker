6a4f8f4c677fb235495a970b0030e63a4913f5e7
Marcin <persky03@gmail.com>
2021-03-20 20:21:59 +0100

    Handle Login Form - frontend

--- frontend/static/src/components/HomeView.js
+++ frontend/static/src/components/HomeView.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import AppBar from './AppBar';
 import AppFooter from './AppFooter';
+import { Link } from 'react-router-dom';
 
 export default class HomeView extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            login: 'demo',
+            password: 'demo'
+        };
+        this.onLoginChange = this.onLoginChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+        this.loginUser = this.loginUser.bind(this);
+    }
+
+    onLoginChange(e) {
+        this.setState({
+            login: e.target.value,
+        });
+    }
+
+    onPasswordChange(e) {
+        this.setState({
+            password: e.target.value,
+        });
+    }
+    loginUser() {
+        console.log(this.state);
     }
     
     render() {
@@ -13,9 +36,9 @@ export default class HomeView extends Component {
             <div class="center-app-box">
                 <div class="main-app-box">
                     <img src="/static/img/logo.png" alt="Issue Tracker"/>
-                    <input placeholder="Login" value="demo" name="login" class="mt-2"/>
-                    <input placeholder="Password" value="demo" name="password" type="password" class="mt-2"/>
-                    <button onClick="" class="btn btn-primary mt-2">Sign In</button>
+                    <input onChange={this.onLoginChange} placeholder="Login" value="demo" name="login" class="mt-2"/>
+                    <input onChange={this.onPasswordChange} placeholder="Password" value="demo" name="password" type="password" class="mt-2"/>
+                    <button onClick={this.loginUser} class="btn btn-primary mt-2">Sign In</button>
                 </div>
             </div>
             <AppFooter />
